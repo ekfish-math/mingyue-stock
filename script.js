c2ecbe9977714bade551b7bff81a4031f4891d1a
ekfish-math <edisonkuo1030@gmail.com>
2026-08-11 16:30:42 +0800

    v0.5 建立股票資料系統

---
+++ script.js
@@ -0,0 +1,35 @@
+const stocks = [
+    {
+        id: "MBC",
+        name: "明月銀行",
+        price: 125.40,
+        change: 3.21,
+        industry: "金融"
+    },
+
+    {
+        id: "KST",
+        name: "京城鋼鐵",
+        price: 87.20,
+        change: 1.82,
+        industry: "工業"
+    },
+
+    {
+        id: "EAS",
+        name: "東海航運",
+        price: 63.80,
+        change: -2.13,
+        industry: "交通"
+    },
+
+    {
+        id: "MTR",
+        name: "明月鐵路",
+        price: 142.70,
+        change: 5.10,
+        industry: "交通"
+    }
+];
+
+console.log(stocks);
